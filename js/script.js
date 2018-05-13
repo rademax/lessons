3ff835cd8d6b0958b102ee00f56350abec8a8e3b
rademax <rademaxbh@gmail.com>
2018-05-13 16:10:13 +0300

    Add ability to select components

--- js/script.js
+++ js/script.js
@@ -12,9 +12,68 @@ let components = new Set();
 let productsHtml = document.getElementsByClassName('products')[0];
 let paginationHtml = document.getElementsByClassName('pagination')[0];
 let filterHtml = document.getElementsByClassName('filter')[0];
-let componentsBaseList = ['Component1', 'Component2', 'Component3', 'Component4'];
+let componentsBaseList = [
+    {name: 'Component 1', price: 12, calories: 75, included: true},
+    {name: 'Component 2', price: 22, calories: 45, included: true},
+    {name: 'Component 3', price: 15, calories: 50, included: false},
+    {name: 'Component 4', price: 20, calories: 60, included: true},
+    {name: 'Component 5', price: 30, calories: 95, included: false},
+    {name: 'Component 6', price: 11, calories: 52, included: true},
+    {name: 'Component 7', price: 10, calories: 72, included: false},
+    {name: 'Component 8', price: 24, calories: 63, included: true},
+    {name: 'Component 9', price: 35, calories: 55, included: false},
+    {name: 'Component 10', price: 35, calories: 55, included: true},
+];
 
 filterHtml.addEventListener('change', function (e) {filter(e.target.value)});
+
+productsHtml.onclick = function (event) {
+    let target = event.target;
+
+    if(target.tagName !== 'LI') {
+        return 0;
+    }
+
+    toggleComponent(target);
+};
+
+function toggleComponent(target) {
+    if(target.classList.contains('excluded-component')) {
+        target.classList.remove('excluded-component');
+        changePriceAndCalories(target, true);
+    }
+    else {
+        target.classList.add('excluded-component');
+        changePriceAndCalories(target, false);
+    }
+}
+
+function changePriceAndCalories(target, increasePrice = true) {
+    let component = target.innerHTML;
+
+    while(target.className !== 'product-content') {
+        target = target.parentNode;
+    }
+
+    let title = target.getElementsByClassName('product-title')[0].innerHTML;
+    let product = {};
+
+    for(let i = 0; i < countProducts; i++) {
+        product = products[i];
+        if(product.title === title) {
+            break;
+        }
+    }
+
+    // let components = getComponentsList();
+    //
+    // for(let component of components) {
+    //     component = product.components[i];
+    //     if(component.name.match(component)) {
+    //
+    //     }
+    // }
+}
 
 class Product {
     constructor (prop) {
@@ -48,10 +107,11 @@ class Product {
         productCalories.className = 'product-calories';
         productPrice.className = 'product-price';
 
-        productTitle.innerHTML = 'Title: ' + this.title;
-        productComponents.innerHTML = 'Components: ' + this.components.join(', ');
-        productCalories.innerHTML = 'Calories: ' + this.calories;
-        productPrice.innerHTML = 'Price: ' + this.price;
+        productTitle.innerHTML = this.title;
+        productComponents.innerHTML = 'Selected components: ';
+        productComponents.appendChild(this.generateProductComponentsList());
+        productCalories.innerHTML = 'Calories: ' + this.formSum('calories');
+        productPrice.innerHTML = 'Price: ' + this.formSum('price');
 
         productContent.appendChild(productTitle);
         productContent.appendChild(productComponents);
@@ -60,6 +120,31 @@ class Product {
 
         productsHtml.appendChild(productItem);
     }
+
+    generateProductComponentsList() {
+        let productComponentsList = document.createElement('ul');
+        for(let i = 0; i < this.components.length; i++) {
+            let component = this.components[i];
+            let productComponentsListItem = document.createElement('li');
+            productComponentsListItem.innerHTML = component.name;
+            if(component.included !== true) {
+                productComponentsListItem.className = 'excluded-component';
+            }
+            productComponentsList.appendChild(productComponentsListItem);
+        }
+        return productComponentsList;
+    }
+
+    formSum(element = 'price') {
+        let sum = this[element];
+        for(let i = 0; i < this.components.length; i++) {
+            let component = this.components[i];
+            if(component['included'] === true) {
+                sum += component[element];
+            }
+        }
+        return sum;
+    }
 }
 
 function generateProducts() {
@@ -67,7 +152,7 @@ function generateProducts() {
         products.push(new Product({
             title: 'Pizza ' + (i+1),
             image: 'images/product.png',
-            components: [randomComponent(), 'Component5', 'Component6', 'Component7'],
+            components: addComponentsToProduct(),
             calories: randomCalories(),
             price: randomPrice()
         }));
@@ -83,8 +168,21 @@ function randomCalories() {
     return Math.floor(Math.random() * (1000 - 500 + 1)) + 500;
 }
 
+function addComponentsToProduct() {
+    let componentsSet = new Set();
+    let componentsList = [];
+    let componentsCount = Math.floor(Math.random() * (7 - 3 + 1)) + 3;
+    for(let i = 0; i < componentsCount; i++) {
+        componentsSet.add(randomComponent());
+    }
+    for(let component of componentsSet) {
+        componentsList.push(component);
+    }
+    return componentsList;
+}
+
 function randomComponent() {
-    let componentId = Math.floor(Math.random() * 4);
+    let componentId = Math.floor(Math.random() * 10);
     return componentsBaseList[componentId];
 }
 
@@ -168,10 +266,10 @@ function createListOfComponents() {
     let componentsList = new Set();
     componentsList.add('No filter');
     for(let i = 0; i < countProducts; i++) {
-        let product = selectedProductsList[i];
-        let componentsCount = selectedProductsList[i].components.length;
+        let product = products[i];
+        let componentsCount = product.components.length;
         for(let j = 0; j < componentsCount; j++) {
-            componentsList.add(product.components[j])
+            componentsList.add(product.components[j].name);
         }
     }
     return componentsList;
@@ -199,9 +297,9 @@ function filter(component) {
     productsFiltered = [];
     for(let i = 0; i < countProducts; i++) {
         let product = products[i];
-        let componentsCount = products[i].components.length;
+        let componentsCount = product.components.length;
         for(let j = 0; j < componentsCount; j++) {
-            if(product.components[j].match(component)) {
+            if(product.components[j].name.match(component)) {
                 productsFiltered.push(product);
                 break;
             }
